97614e4183279e8412ade86f91d15dcb6999551d
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2025-02-04 09:33:44 -0300

    feat: create calculate function

---
+++ assets/scripts/math.script.js
@@ -0,0 +1,12 @@
+function calcular() {
+    let numA = Number(document.getElementById("numA").value);
+    let numB = Number(document.getElementById("numB").value);
+
+    //alert(numA);
+    //alert(numB);
+
+    // Soma
+    let result = numA + numB;
+
+    document.getElementById("result").innerText = result;
+}
