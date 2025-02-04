054390e8580dce059be50d0e6b517b5fb30cc087
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2025-02-04 11:36:24 -0300

    feat: create more functions math

--- assets/scripts/index.script.js
+++ assets/scripts/index.script.js
@@ -1,6 +1,6 @@
 function exibirNome() {
     let nome = document.getElementById("nome").value;
-    
+
     //console.log("Nome digitado: ", nome);
-    document.getElementById("msg").innerHTML = nome;
+    document.getElementById("msg").innerHTML = `Olá, ${nome}`;
 }
--- assets/scripts/math.script.js
+++ assets/scripts/math.script.js
@@ -1,4 +1,4 @@
-function calcular() {
+function soma() {
     let numA = Number(document.getElementById("numA").value);
     let numB = Number(document.getElementById("numB").value);
 
@@ -10,3 +10,31 @@ function calcular() {
 
     document.getElementById("result").innerText = result;
 }
+
+function sub() {
+    let numA = Number(document.getElementById("numA").value);
+    let numB = Number(document.getElementById("numB").value);
+
+    let result = numA - numB;
+
+    document.getElementById("result").innerText = result;
+}
+
+function mult() {
+    let numA = Number(document.getElementById("numA").value);
+    let numB = Number(document.getElementById("numB").value);
+
+    let result = numA * numB;
+
+    document.getElementById("result").innerText = result;
+}
+
+function divi() {
+    let numA = Number(document.getElementById("numA").value);
+    let numB = Number(document.getElementById("numB").value);
+
+    let result = numA / numB;
+
+    document.getElementById("result").innerText = result;
+}
+
